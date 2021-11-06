5b07eab5dc29653af3cafda01b475961463396fa
JaimeMadFer <61852686+JaimeMadFer@users.noreply.github.com>
2021-11-06 12:52:06 +0100

    1

---
+++ practica 2.2/ejercicio1.c
@@ -0,0 +1,10 @@
+ls
+-a: muestra todos los archivos y directorios incluyendo los que empiezan por '.'
+-l: muestra varias características de los ficheros y directorios
+-d: muestra el directorio en el que nos encontramos
+-h: muestra los archivos y directorios en un formato legible
+-i: muestra el i-nodo de los archivos y directorios
+-R: muestra recursivamente el contenido de los directorios que se listan
+-1: muestra una entrada por línea
+-F: muestra un símbolo al final del nombre de cada entrada según su tipo
+--color: da la opción de mostrar la salida con o sin colores
---
+++ practica 2.2/ejercicio2.c
@@ -0,0 +1,14 @@
+[cursoredes@localhost ~]$ ls -ld
+drwx------. 19 cursoredes cursoredes 4096 Nov  6 12:06 .
+
+[cursoredes@localhost ~]$ ls -ld Desktop
+drwxr-xr-x 2 cursoredes cursoredes 116 Sep  9  2018 Desktop
+
+[cursoredes@localhost ~]$ ls -ld /
+dr-xr-xr-x. 17 root root 224 Sep  3  2018 /
+
+[cursoredes@localhost ~]$ ls -ld /root
+dr-xr-x---. 7 root root 235 Jan  9  2020 /root
+
+[cursoredes@localhost ~]$ ls -ld /tmp
+drwxrwxrwt. 12 root root 4096 Nov  6 11:17 /tmp
