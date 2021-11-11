5ac93f8ac761081afb2d3a9fa2e67693ad97cbbb
JaimeMadFer <61852686+JaimeMadFer@users.noreply.github.com>
2021-11-11 20:15:21 +0100

    5

---
+++ practica 2.2/ejercicio13.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char **argv)
+{
+        int old_fd, new_fd, fd_errores;
+        char *s;
+
+        old_fd = open(argv[1], O_RDWR | O_CREAT | O_TRUNC, 0777);
+        new_fd = dup2(old_fd, 1);
+        fd_errores = dup2(old_fd, 2);
+
+        printf("Salida estándar:\n", argv[1]);
+        printf("Esta es la salida, no hay más\n", argv[1]);
+
+       if (setuid(0) == -1)
+       {
+                perror(s);
+       }
+
+		dup2(new_fd, old_fd);
+        dup2(fd_errores, old_fd);
+
+        return 0;
+}
+
+// ls > dirlist 2>&1: la salida estándar y de errores pasan a estar en dirlist
+// ls 2>&1 > dirlist: salida de errores en salida estándar y salida estándar en dirlist
---
+++ practica 2.2/ejercicio14.c
@@ -0,0 +1,10 @@
+1: POSIX  ADVISORY  WRITE 1967 fd:00:684649 0 EOF
+
+1 :				posición en la lista
+POSIX :			tipo de cerrojo
+ADVISORY :		restrición del cerrojo
+WRITE :			tipo de uso que se hace del cerrojo
+1967 :			pid del proceso
+fd:00:684649 :	major, minor y nº de inodo del fichero bloqueado
+0 :				byte de desplazamiento del 1er byte del cerrojo
+EOF :			byte de desplazamiento del último byte del cerrojo
